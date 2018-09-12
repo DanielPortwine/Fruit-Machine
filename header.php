8d0eb9af1d92bc94e762ac27733328e65d497eda
Dan Portwine <dan.portwine@protonmail.com>
2018-09-12 19:51:19 +0100

    Added ideas and changed header
    Added a new file of ideas for the game. Added relevant links to the nav bar. Relocated the nav bar from index to header.

--- header.php
+++ header.php
@@ -11,3 +11,14 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+	<a href="index.php" class="navbar-brand">Fruit Machine</a>
+	<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+		<div class="navbar-nav">
+			<a class="nav-item nav-link active" href="game.php">Play</a>
+			<a class="nav-item nav-link" href="leaderboard.php">Leaderboard</a>
+			<a class="nav-item nav-link" href="stats.php">Stats</a>
+			<a class="nav-item nav-link" href="edit.php">Edit</a>
+		</div>
+	</div>
+</nav>
--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
 require_once('header.php');
 ?>
-<nav class="navbar navbar-dark bg-dark">
-	<p class="navbar-brand">Hello world</p>
-</nav>
+
 <?php
-require_once('footer.php');
+require_once('footer.html');
 ?>
