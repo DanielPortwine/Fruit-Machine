de6d660e3e0d34d5b329b47c5c2c9719befbc679
Dan Portwine <dan.portwine@protonmail.com>
2018-09-19 20:53:56 +0100

    Item positions
    Setup the containers for the items in play. Created pineapple and strawberry images.

--- play.php
+++ play.php
@@ -6,3 +6,24 @@ if (empty($_SESSION['username'])) {
 	echo '<script>window.location = "index.php";</script>';
 }
 ?>
+<div class="vertical-center">
+	<div class="container text-center">
+	  <div class="row">
+		<div class="col-sm">
+		  <img src="images/pineapple.png">
+		</div>
+		<div class="col-sm">
+		  <img src="images/strawberry.png">
+		</div>
+		<div class="col-sm">
+		  <img src="images/pineapple.png">
+		</div>
+		<div class="col-sm">
+		  <img src="images/strawberry.png">
+		</div>
+		<div class="col-sm">
+		  <img src="images/pineapple.png">
+		</div>
+	  </div>
+	</div>
+</div>
