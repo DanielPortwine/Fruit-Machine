7f65c476c57f942086fb8eb0c80e4bb862c157c7
Dan Portwine <dan.portwine@protonmail.com>
2018-09-20 02:55:08 +0100

    favicon
    added a favicon

--- header.php
+++ header.php
@@ -20,6 +20,7 @@ if (isset($_SESSION['alert'])) {
 	<title>Fruit machine</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="icon" href="images/begin.png">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
