ddefadf71b05ee5aeb3bf8534f050ef396021d51
Dan Portwine <dan.portwine@protonmail.com>
2018-09-24 23:56:54 +0100

    Phantom item bug fixed
    Bug fix: fixed non-value item from appearing.

--- spin.php
+++ spin.php
@@ -6,9 +6,9 @@ unset($items[0]);
 unset($items[1]);
 if (mysqli_fetch_row($conn->query("SELECT spinsLeft FROM users WHERE username = '{$_SESSION['username']}';"))[0] > 0){
 	for ($i=0;$i<4;$i++){
-		echo mt_rand(0,sizeOf($items)) . ',';
+		echo mt_rand(0,sizeOf($items)-1) . ',';
 	}
-	echo mt_rand(0,sizeOf($items));
+	echo mt_rand(0,sizeOf($items)-1);
 } else {
 	$_SESSION['alert'] = 'No spins left!';
 	$_SESSION['alert-type'] = 'danger';
