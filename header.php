4ac8d2cc949a1a8eb474fe2ca2b0030d2255b5c3
Dan Portwine <dan.portwine@protonmail.com>
2019-02-09 21:19:20 +0000

    testing staging redirect

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <?php
 echo $_SERVER['HTTP_HOST'];
 if ($_SERVER['HTTP_DPTESTING'] == 'true' && $_SERVER['HTTP_HOST'] != 'staging.danportwine.co.uk') {
-    echo '<script>window.location = "http://staging.danportwine.co.uk/' . $_SERVER['REQUEST_URI'] . '";</script>';
+    echo '<script>window.location = "staging.danportwine.co.uk/' . $_SERVER['REQUEST_URI'] . '";</script>';
 }
 
 require_once('connection.php');
